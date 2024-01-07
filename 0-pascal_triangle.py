12f04415c3d704d3dce8f39a9fe1767b40f81bab
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-07 15:49:12 +0100

    alx interview

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Pascal Triangle """
+
+
+def pascal_triangle(n):
+    """Implements pascal triangle"""
+    pascal_tri = [[1]]
+    for i in range(n - 1):
+        tri = list(range(i + 2))
+        tri_size = len(pascal_tri[i])
+        for j in range(tri_size):
+            right_val = 0
+            left_val = 0
+            cur_val = pascal_tri[i][j]
+            if (j - 1) >= 0:
+                left_val = pascal_tri[i][j - 1]
+            if (j + 1) < tri_size:
+                right_val = pascal_tri[i][j + 1]
+            tri[j] = left_val + cur_val
+            tri[j + 1] = right_val + cur_val
+        pascal_tri.append(tri)
+    return (pascal_tri)
