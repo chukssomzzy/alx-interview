87f4d37673f242f8ff9af822d7a1923a9cdeb710
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-01-20 12:02:50 +0100

    factor

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 """Find minimum operation"""
 import math
+from typing import Tuple, Union
 
 
 def minOperations(n: int) -> int:
     """GET THE MINIMUM OPERATIONS"""
+    fac: Union[Tuple[int, int], None]
     if n == 1 or n == 0:
         return 0
+
     if not n % 2:
         ops = minOperations(math.floor(n / 2)) + 2
     elif math.floor(math.sqrt(n)) ** 2 == n:
         ops = minOperations(math.floor(math.sqrt(n))) \
             + math.floor(math.sqrt(n))
+    elif fac := factor(n):
+        ops = minOperations(fac[1]) + fac[0]
     else:
         ops = n
     return ops
+
+
+def factor(n: int) -> Union[Tuple[int, int], None]:
+    """Get all factors of a number"""
+
+    for i in range(2, math.floor(math.sqrt(n))):
+        for j in range(2, math.floor(n / 2)):
+            if i * j == n:
+                return (i, j)
+    return None
