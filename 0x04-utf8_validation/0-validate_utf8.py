3b7164abac8c8d846737c60af3027a943a9e50bc
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-01 14:47:57 +0000

    implemented: validator for utf8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Validate utf8"""
+
+
+from typing import List
+import pdb
+# pdb.set_trace()
+
+
+def validUTF8(data: List[int]) -> bool:
+    """Validate utf8"""
+    b_seen = 0
+    b_expec = 0
+    b_len = 8
+    for code_point in data:
+        if code_point >= 256:
+            return False
+        b = f"{code_point:08b}"
+        for i in range(b_len):
+            current_b = int(b[i], base=2)
+            next_b = 0
+            if i < (b_len - 2):
+                next_b = int(b[i + 1], base=2)
+            if not i and not current_b and not b_expec:
+                break
+            elif not i and current_b and not next_b:
+                b_seen += 1
+                break
+            elif b_expec < 8 and not b_seen and current_b:
+                b_expec += 1
+            else:
+                return False
+            if b_seen == b_expec:
+                b_seen = 0
+                b_expec = 0
+                break
+    return True
