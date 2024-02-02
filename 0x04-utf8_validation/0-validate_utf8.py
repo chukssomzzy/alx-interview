ffb2161eacc694facf034bf53e85926c2bfc16ae
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-02 14:24:55 +0000

    failing on something

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,13 +11,15 @@ def validUTF8(data: List[int]) -> bool:
 
     for byte in data:
         if not n_byte:
-            if byte >> 5 == 0b110:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
                 n_byte = 1
             elif byte >> 4 == 0b1110:
                 n_byte = 2
             elif byte >> 3 == 0b11110:
                 n_byte = 3
-            elif byte >> 7 != 0:
+            else:
                 return False
         else:
             if byte >> 6 != 0b10:
