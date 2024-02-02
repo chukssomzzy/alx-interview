20ba07ffefe7e98f9528cc8370ee3007e21b82de
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-02-02 13:11:23 +0000

    all but

--- 0x04-utf8_validation/0-main.py
+++ 0x04-utf8_validation/0-main.py
@@ -13,18 +13,18 @@ validUTF8 = __import__('0-validate_utf8').validUTF8
 # data = [229, 65, 127, 256]
 # assert validUTF8(data) is False  # Should print False
 #
-# # Test Case 4: Valid 2-byte character
-# data = [194, 128]
-# assert validUTF8(data) is True  # Should print True
-#
-# # Test Case 5: Valid 3-byte character
-# data = [226, 152, 131]
-# assert validUTF8(data) is True  # Should print True
-#
-# # Test Case 6: Valid 4-byte character
-# data = [240, 159, 152, 132]
-# assert validUTF8(data) is True  # Should print True
-#
+# Test Case 4: Valid 2-byte character
+data = [194, 128]
+assert validUTF8(data) is True  # Should print True
+
+# Test Case 5: Valid 3-byte character
+data = [226, 152, 131]
+assert validUTF8(data) is True  # Should print True
+
+# Test Case 6: Valid 4-byte character
+data = [240, 159, 152, 132]
+assert validUTF8(data) is True  # Should print True
+
 # Test Case 7: Invalid 4-byte character (exceeding limit)
 data = [240, 159, 152, 132, 65]
 assert validUTF8(data) is False  # Should print False
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,28 +8,25 @@ from typing import List
 def validUTF8(data: List[int]) -> bool:
     """Validate utf8"""
     n_byte = 0
-    c_ra_str = 128
-    c_ra_end = 191
     max_byte = 247
 
     for byte in data:
         if byte > max_byte:
             return False
         if not n_byte:
-            if byte >> 7 == 0:
-                continue
-            elif byte >> 5 == 0b110 and c_ra_str > byte < c_ra_end:
+            if byte >> 7 != 0:
+                return False
+            elif byte >> 5 == 0b110:
                 n_byte += 1
-            elif byte >> 4 == 0b1110 and c_ra_str > byte < c_ra_end:
+            elif byte >> 4 == 0b1110:
                 n_byte += 2
-            elif byte >> 3 == 0b11110 and c_ra_str > byte < c_ra_end:
+            elif byte >> 3 == 0b11110:
                 n_byte += 3
-            elif byte >> 2 == 0b111110 and c_ra_str > byte < c_ra_end:
+            elif byte >> 2 == 0b111110:
                 n_byte += 4
             else:
                 return False
         else:
-            print(n_byte)
             if byte >> 6 == 0b10 and n_byte:
                 n_byte -= 1
             else:
