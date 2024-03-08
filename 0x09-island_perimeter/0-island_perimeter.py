e6df98cc7e369e627be1b1e74fd4f52ee104e6ad
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-08 05:21:23 +0100

    solution better than chatgpt

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,13 +5,28 @@
 
 
 def island_perimeter(grid):
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    """Find island perimeter
+    Args:
+        grid (list of list): grid
+    """
+    island = 0
+    for i in range(1, len(grid) - 1, 1):
+        for j in range(1, len(grid[0]) - 1, 1):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-    return perimeter
+                if island:
+                    return 0
+                island = find_island(grid, i, j)
+    return (island + 1) * 2
+
+
+def find_island(grid, i, j):
+    """Find connected island"""
+    if i == len(grid) - 1 or i == 0 or j == 0 or\
+            j == len(grid[0]) - 1 or grid[i][j] == 2:
+        return 0
+    if not grid[i][j] or grid[i][j] == 2:
+        return 0
+    grid[i][j] = 2
+    return (1 + find_island(grid, i - 1, j) + find_island(
+        grid, i + 1, j) + find_island(grid, i, j + 1) + find_island(
+            grid, i, j - 1))
