2a484ea3c40bba7ccf53c589f79c1d3061385206
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-15 05:38:52 +0100

    prime game

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,47 +1,31 @@
 #!/usr/bin/python3
 
-
 """Find island perimeter"""
 
 
 def island_perimeter(grid):
     """Find island perimeter
     Args:
-        grid (list of list): grid representing a map of land (1) and water (0)
-    Returns:
-        int: Perimeter of the island
+        grid (list of list): grid
     """
-    rows, cols = len(grid), len(grid[0])
-    visited = [[False for _ in range(cols)] for _ in range(rows)]  # Kee
+    island = 0
+    for i in range(1, len(grid) - 1, 1):
+        for j in range(1, len(grid[0]) - 1, 1):
+            if grid[i][j] == 1:
+                if island:
+                    return 0
+                island = find_island(grid, i, j)
+    return island * 2
 
-    def dfs(i, j):
 
-        """
-        Depth-First Search to explore connected island cells
-        """
-        if (i < 0 or i >= rows or j < 0 or j >= cols or (grid[i][j] == 0
-                                                         ) or visited[i][j]):
-            return 0  # Reached water or visited cell, no perimeter adde
-        visited[i][j] = True  # Mark cell as visited
-
-        perimeter = 0  # Initialize perimeter for this island branch
-        perimeter += 1 if i == 0 or grid[i - 1][j] == 0 else 0
-        perimeter += 1 if i == rows - 1 or grid[i + 1][j] == 0 else 0
-        perimeter += 1 if j == 0 or grid[i][j - 1] == 0 else 0
-        perimeter += 1 if j == cols - 1 or grid[i][j + 1] == 0 else 0  #
-
-        # Explore unvisited connected land cells recursively
-        perimeter += dfs(i - 1, j)
-        perimeter += dfs(i + 1, j)
-        perimeter += dfs(i, j - 1)
-        perimeter += dfs(i, j + 1)
-
-        return perimeter
-
-    # Find a starting land cell and perform DFS
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1 and not visited[i][j]:
-                return dfs(i, j)
-
-    return 0
+def find_island(grid, i, j):
+    """Find connected island"""
+    if i == len(grid) - 1 or i == 0 or j == 0 or\
+            j == len(grid[0]) - 1 or grid[i][j] == 2:
+        return 0
+    if not grid[i][j] or grid[i][j] == 2:
+        return 0
+    grid[i][j] = 2
+    return (1 + find_island(grid, i - 1, j) + find_island(
+        grid, i + 1, j) + find_island(grid, i, j + 1) + find_island(
+            grid, i, j - 1))
